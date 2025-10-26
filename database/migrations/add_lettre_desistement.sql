5f17c6def048076a20152ece841168f77d121c86
ManMbaiLikol <kamerdesignstyle@gmail.com>
2025-10-26 22:00:20 +0100

    Fix: Add SQL_MODE for migration

--- database/migrations/add_lettre_desistement.sql
+++ database/migrations/add_lettre_desistement.sql
@@ -2,5 +2,7 @@
 -- Date : 2025-10-26
 -- Description : Ajout du document "Lettre de désistement" pour les stations-services
 
+SET SQL_MODE='ALLOW_INVALID_DATES';
+
 ALTER TABLE fiches_inspection
 ADD COLUMN lettre_desistement BOOLEAN DEFAULT FALSE COMMENT 'Lettre de désistement disponible' AFTER plan_masse;
